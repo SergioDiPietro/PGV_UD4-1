3c342d14db173568b63ee29863bf8582fceed447
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2022-02-28 23:12:21 +0000

    Feat: Generada solicitud de datos al usuario.

--- src/com/dipisoft/Main.java
+++ src/com/dipisoft/Main.java
@@ -1,8 +1,28 @@
 package com.dipisoft;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("----- Datos para la conexión ----- ");
+        System.out.print("Dirección IP o nombre de dominio: ");
+        String address = sc.nextLine();
+
+        System.out.print("Puerto: ");
+        int port = Integer.parseInt(sc.nextLine());
+
+        System.out.println("\n----- Datos para acceso ----- ");
+        System.out.print("Usuario: ");
+        String username = sc.nextLine();
+
+        System.out.print("Contraseña: ");
+        String password = sc.nextLine();
+
+        System.out.println("\n" + address + ":" + port + ", " + username + ":" + password);
+
+        sc.close();
     }
 }
